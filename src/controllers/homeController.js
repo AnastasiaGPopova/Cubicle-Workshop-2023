a3da17f4d0eaccfe6025bb28838e8078bc5bdf19
AnastasiaGPopova <ana.popova.register@gmail.com>
2023-01-21 13:10:02 +0200

    adding the Search Option

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,29 @@
 const db = require('../db.json')
 
 exports.getHomePage = (req,res) => {
-    res.render('index', {cubes: db.cubes})
+    //console.log(req.query) //express is doing this for us
+    const {search, from, to } = req.query
+
+    let cubes = db.cubes
+
+    if(search) {
+        cubes = cubes.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase())) //this will filter all cubes, that include the search key word
+        
+    }
+
+    if(from) {
+        cubes = cubes.filter(cube => cube.difficultyLevel >= from)
+    }
+
+    
+    if(to) {
+        cubes = cubes.filter(cube => cube.difficultyLevel <= to)
+    }
+    //we do this in separate "if-s" and not with "if-else", because we do not know what the user will use - Only Name, Only from or Only to. 
+    //This way it will works only with name, or only with from, or with name + from etc
+
+
+    res.render('index', {cubes, search})
 }
 
 exports.getAboutPage = (req,res) => {
