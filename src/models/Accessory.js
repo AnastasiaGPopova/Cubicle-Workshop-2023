bf61caddd8995d16d682389f7c8f70c07a2f985c
AnastasiaGPopova <ana.popova.register@gmail.com>
2023-01-29 18:01:14 +0200

    added validation to accessories

--- src/models/Accessory.js
+++ src/models/Accessory.js
@@ -7,7 +7,8 @@ const asseccorySchema = new mongoose.Schema({
     }, 
     imageUrl: {
         type: String,
-        required: true
+        required: true,
+        match: [/^https?:\/\//, "Invalid URL"]
     },
     description: {
         type: String,
